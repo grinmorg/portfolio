53dc5c9a5093087734525e02e90f9eecebfd3ff9
Grigory Morgachev <grinmorg@gmail.com>
2022-06-13 14:22:56 +0300

    update

--- src/views/Cases.vue
+++ src/views/Cases.vue
@@ -3,17 +3,10 @@
     <div class="container">
       <h2 class="title">Портфолио</h2>
       <ul class="cases__list">
-        <li
-          class="cases__item case"
-          style="grid-row: span 6"
-          ref="case"
-          v-for="(item, idx) in cases"
-          :key="idx"
-          v-tilt="{
-            perspective: 1200,
-            scale: 1.05,
-          }"
-        >
+        <li class="cases__item case" style="grid-row: span 6" ref="case" v-for="(item, idx) in cases" :key="idx" v-tilt="{
+          perspective: 1200,
+          scale: 1.05,
+        }">
           <a :href="item.url" target="_blank" class="case__link">
             <div v-if="!item.ready" class="case__dev">
               {{ $t("base.indev") }}
@@ -27,19 +20,15 @@
             <p class="case__name" v-if="item.name">{{ item.name }}</p>
             <p class="case__desc" v-if="item.desc">{{ item.desc }}</p>
             <ul class="tags">
-              <li
-                class="tags__item"
-                v-for="(tag, idxTag) in item.tags"
-                :key="idxTag"
-              >
+              <li class="tags__item" v-for="(tag, idxTag) in item.tags" :key="idxTag">
                 <span v-if="tag === 'Многостраничный сайт'">{{
-                  $t("base.multipage")
+                    $t("base.multipage")
                 }}</span>
                 <span v-else-if="tag === 'Сайт-каталог'">{{
-                  $t("base.catalog")
+                    $t("base.catalog")
                 }}</span>
                 <span v-else-if="tag === 'Сайт-сервис'">{{
-                  $t("base.service")
+                    $t("base.service")
                 }}</span>
                 <span v-else>{{ tag }}</span>
               </li>
@@ -84,7 +73,7 @@ export default {
       {
         img: IT159,
         name: "IT 159",
-        desc: "Интернет-магазин для провайдера интеренета. Реализован функционал коризны и оформления заказа с онлайн-оплатой",
+        desc: "Интернет-магазин для провайдера интеренета. Реализован функционал корзины и оформления заказа с онлайн-оплатой",
         tags: [
           "Интернет-магазин",
           "WordPress",
@@ -123,6 +112,7 @@ export default {
       {
         img: workVzamke24,
         name: "VZAMKE24",
+        desc: "Лендинг с системой мультилендинга для предоставления услуг в разных городах",
         tags: ["Многостраничный сайт", "WordPress", "JQuery", "SASS", "Gulp"],
         url: "https://vzamke24.ru/",
         ready: true,
@@ -130,6 +120,7 @@ export default {
       {
         img: workInrosFirst,
         name: "INROS Окна",
+        desc: "Многостраничник для компании предоставляющей услуги по остеклению",
         tags: ["Многостраничный сайт", "WordPress", "JQuery", "SASS", "Gulp"],
         url: "https://msk1.inros-gk.ru/",
         ready: true,
@@ -137,6 +128,7 @@ export default {
       {
         img: workRemont24,
         name: "REMONT24",
+        desc: "Лендинг с системой мультилендинга для предоставления услуг в разных городах",
         tags: ["Многостраничный сайт", "WordPress", "JQuery", "SASS", "Gulp"],
         url: "https://remontgenerator.ru/",
         ready: true,
@@ -153,6 +145,7 @@ export default {
   margin-top: 150px;
   overflow: hidden;
   padding-bottom: 50px;
+
   &__list {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
@@ -189,6 +182,7 @@ export default {
     width: 100%;
     color: var(--white);
   }
+
   &__img {
     overflow: hidden;
     height: 250px;
@@ -236,6 +230,7 @@ export default {
       grid-template-columns: 1fr;
     }
   }
+
   .title {
     text-align: center;
   }
